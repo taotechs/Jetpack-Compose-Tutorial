3e54eceb236e215936219ed09da79933c4279c3a
TAOFEEK AKINTUNDE <58163594+taotechs@users.noreply.github.com>
2022-10-28 09:37:53 +0100

    jetpack compose tutorial

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnTogetherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TutorialImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun JetpackCompose() {
+
+    Column {
+        Text(
+            text = "Jetpack Compose tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 16.dp, bottom = 16.dp, end = 16.dp),
+        )
+        Text(
+            text =  stringResource(R.string.AboutJetPackCompose1),
+            modifier = Modifier
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.AboutJetpackCompose2),
+            modifier = Modifier
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 16.dp, bottom = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+
+}
+
+@Composable
+fun TutorialImage() {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        JetpackCompose()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LearnTogetherTheme {
+        TutorialImage()
+    }
+}
